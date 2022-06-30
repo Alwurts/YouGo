dd7790cc76809e97b0c477d1d48f2d7732955909
Alejandro Wurts <32560488+Alwurts@users.noreply.github.com>
2022-06-29 23:25:12 -0500

    Countdown added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
-import styled, { keyframes, ThemeProvider } from "styled-components";
+import React, { useRef, useState, useEffect } from 'react';
+import styled from "styled-components";
 import './App.css';
 
-const Container = styled.div`
+interface ContainerProps {
+  colorWinner: String | undefined,
+}
+
+const Container = styled.div<ContainerProps>`
   position: static;
   height: 100vh;
   width: 100vw;
-  background: black;
+  background: ${({colorWinner})=> colorWinner ? `${colorWinner}` : "black"};
   touch-action: none;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TimerMessage = styled.p`
+
+  color: gray;
+  font-size: 200px;
 `;
 
 interface touchPosition {
@@ -21,31 +35,74 @@ function App() {
 
   const [touches, setTouches] = useState<any>({})
 
+  const [winner, setWinner] = useState<touchPosition>()
+  let timerWinner = useRef<any>()
+
+  const [countToRandom, setCountToRandom] = useState<number>(0)
+  const countToRandomRef = useRef(countToRandom)
+  countToRandomRef.current = countToRandom;
+  
+
+  const COLORS : any = {
+    0:"hsl(359, 100%, 50%)",
+    1:"hsl(96, 100%, 50%)",
+    2:"hsl(208, 100%, 50%)",
+    3:"hsl(39, 100%, 50%)",
+    4:"hsl(300, 100%, 50%)",
+    5:"hsl(68, 100%, 50%)",
+    6:"hsl(156, 100%, 50%)",
+    7:"hsl(330, 100%, 50%)",
+    8:"hsl(244, 100%, 50%)",
+    9:"hsl(60, 100%, 50%)",
+    10:"hsl(178, 100%, 50%)",
+    11:"hsl(90, 100%, 50%)",
+    12:"hsl(23, 100%, 50%)",
+    13:"hsl(272, 100%, 50%)",
+    14:"hsl(121, 100%, 50%)",
+    15:"hsl(48, 100%, 50%)",
+    16:"hsl(359, 100%, 50%)",
+    17:"hsl(96, 100%, 50%)",
+    18:"hsl(208, 100%, 50%)",
+    19:"hsl(39, 100%, 50%)",
+    20:"hsl(300, 100%, 50%)",
+  }
+
+  const onWinnerTimer = (tempTouches: any) => {
+
+    if (countToRandomRef.current >= 3) {
+      setWinner(tempTouches[0])
+      return
+    }
+    setCountToRandom(prevCount => prevCount + 1)
+  }
+  
   const touchStart = (event: React.TouchEvent<HTMLDivElement>) => {
 
     let tempTouches = {...touches}
 
-    Object.entries(event.changedTouches).map(([, touch]) => {
-
+    for (let [, touch] of Object.entries(event.changedTouches)) {
       let newTouch : touchPosition = {
-        color: `hsl(${Math.floor(Math.random() * 359)}, 100%, 50%)`,
+        color: `${COLORS[touch.identifier]}`,
         identifier: touch.identifier,
         positionX: touch.clientX,
         positionY: touch.clientY,
       }
 
       tempTouches[touch.identifier] = newTouch
-    })
+    }
 
-
-    
     setTouches(tempTouches)
+
+    timerWinner.current = setInterval(()=>{
+      onWinnerTimer(tempTouches);
+    }, 1000);
   }
 
   const touchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+
     let tempTouches = {...touches}
 
-    Object.entries(event.changedTouches).map(([, touch]) => {
+    for (let [, touch] of Object.entries(event.changedTouches)) {
 
       let newTouch : touchPosition = {
         color: tempTouches[touch.identifier].color,
@@ -55,7 +112,7 @@ function App() {
       }
 
       tempTouches[touch.identifier] = newTouch
-    })
+    }
 
     setTouches(tempTouches)
   }
@@ -64,21 +121,35 @@ function App() {
 
     let tempTouches = {...touches}
 
-    Object.entries(event.changedTouches).map(([, touch]) => {
+    for (let [, touch] of Object.entries(event.changedTouches)) {
       delete tempTouches[touch.identifier]
-    })
+    }
 
     setTouches(tempTouches);
+
+    clearCountDown();
+  }
+
+  const clearCountDown = () => {
+
+    clearInterval(timerWinner.current)
+    setWinner(undefined)
+    timerWinner.current=undefined
+    setCountToRandom(0)
+    countToRandomRef.current = 0
+
   }
 
   return (
     <Container 
       className="App"
+      colorWinner={winner?.color}
       onTouchStart={e => touchStart(e)}
       onTouchMove={e => touchMove(e)}
       onTouchEnd={e => touchEnd(e)}
       onTouchCancel={e => touchEnd(e)}
     >
+      {(!winner && timerWinner.current) && <TimerMessage>{countToRandom}</TimerMessage>}
       {Object.keys(touches).length !== 0 && Object.entries(touches).map(([index, element]:Array<any>)=>(
               
         <div  
@@ -87,9 +158,9 @@ function App() {
             top: `${parseInt(element.positionY)-60}px`, 
             left: `${parseInt(element.positionX)-60}px`,
             borderTop: "12px solid black",
-            borderRight: "12px solid #d1d1d1",
+            borderRight: `12px solid ${winner ? "black" : "#d1d1d1"}`,
             borderBottom: "12px solid black",
-            borderLeft: "12px solid #d1d1d1",
+            borderLeft: `12px solid ${winner ? "black" : "#d1d1d1"}`,
             background: `${element.color}`,
             width: "120px",
             height: "120px",
